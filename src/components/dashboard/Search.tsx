62895a0bfcc285631e98d6aeafc538e8e09556de
Elvio Moretti <elvio.moretti@alkemy.com>
2023-08-05 00:06:13 +0200

    add element on dashboard

---
+++ src/components/dashboard/Search.tsx
@@ -0,0 +1,20 @@
+export const Search = (props: {search: string, setSearch: (arg: string) => void}) => {
+
+    const handleSearch = (event: string) => {
+        props.setSearch(event);
+    }
+
+    return (
+        <div className='w-full h-[50px] my-4 '>
+            <div className='flex flex-row w-full h-full rounded-lg overflow-hidden relative items-center bg-eerie-black '>
+                <svg className="w-6 h-6 absolute my-auto pl-1 " xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" >
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
+                </svg>
+                <input value={props.search} onChange={(e) => handleSearch(e.target.value) } className='w-full h-full pl-8 bg-eerie-black pr-6' type='text' placeholder='Search'/>
+                <svg onClick={() => handleSearch('')} className="w-6 h-6 absolute right-0 pr-2 cursor-pointer" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" >
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M9.75 9.75l4.5 4.5m0-4.5l-4.5 4.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                </svg>
+            </div>
+        </div>
+    );
+}
--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,22 +1,43 @@
-import { auth } from '../config/firebase';
+import  { auth } from '../config/firebase';
 import { useNavigate } from 'react-router-dom';
 import { getRoutePath } from '../utils/Routes';
 import { RoutesName } from '../interfaces/Routes.interface';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import { Hello } from '../components/dashboard/Hello';
+import firebase from 'firebase/compat/app';
+import { Search } from '../components/dashboard/Search';
+
 
 export const DashboardPage = () => {
     const navigate = useNavigate();
+    const [currentUser, setCurrentUser] = useState<firebase.User>();
+    const [search, setSearch] = useState('');
     const logOut = () => {
         auth.signOut().then( _ => navigate(getRoutePath(RoutesName.LOGIN)));
     }
     useEffect(() => {
-        console.log(auth.currentUser?.displayName)
-    }, [])
+        console.log(auth.currentUser)
+        if(auth.currentUser !== null){
+            setCurrentUser(auth.currentUser);
+        }
+    }, []);
+
+    const handleSearch = (value: string) => {
+        setSearch(value);
+    }
 
     return(
-        <div>
-            <h3>Dashboard Page</h3>
-            <button onClick={logOut}>LOGOUT</button>
+        <div className='flex flex-row text-silver bg-eerie-black h-screen w-full -m-4'>
+            <div className='flex flex-col w-1/4 bg-jet h-full px-2'>
+                <div className='flex flex-row justify-between my-4 items-center '>
+                    <Hello displayName={currentUser?.displayName} image={currentUser?.photoURL} />
+                </div>
+                <Search search={search} setSearch={handleSearch} />
+                <h3>Dashboard Page</h3>
+                <button onClick={logOut}>LOGOUT</button>
+            </div>
+            <div className='flex flex-col w-1/4 border-r-silver border-r-2'></div>
+            <div className='flex flex-col w-2/4'></div>
         </div>
     );
 }
