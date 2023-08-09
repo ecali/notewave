2a167d429e2f6fd9c9c6c744958418df5dafdf1b
Elvio Moretti <elvio.moretti@alkemy.com>
2023-08-09 10:10:59 +0200

    minor

--- src/utils/Validators.ts
+++ src/utils/Validators.ts
@@ -1,11 +1,9 @@
 const REGEX_EMAIL = /^[0-9a-zA-Z]+([0-9a-zA-Z]*[-._+])*[0-9a-zA-Z]+@[0-9a-zA-Z]+([-.][0-9a-zA-Z]+)*([0-9a-zA-Z]*[.])[a-zA-Z]{2,6}$/
 const REGEX_CAPITAL = /[A-Z]/
 export const isValidEmail = (email: string) => {
-    console.log('email', REGEX_EMAIL.test(email) && email !== '' )
     return REGEX_EMAIL.test(email) && email !== '';
 }
 
 export const isPasswordValid = (password: string) => {
-    console.log('pass', password.length > 6 && REGEX_CAPITAL.test(password) && password !== '')
     return password.length > 6 && REGEX_CAPITAL.test(password) && password !== '';
 }
